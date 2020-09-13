0c6055014c677f7f38ae34a518700b5915b829c4
Will B Chang <willbeethoven@gmail.com>
2020-09-13 03:19:44 +0000

    test(fix): chagne home_path to root_path

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get home_path
+    get root_path
     assert_response :success
     assert_select "title", @base_title
   end
