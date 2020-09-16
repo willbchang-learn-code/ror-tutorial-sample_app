d856667fbeec95f0ac58bacaa3fbbb9fb97b1c80
Will B Chang <will.b.chang@gmail.com>
2020-09-16 15:20:20 +0800

    rubocop: replace " with '

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: 'Example User', email: 'user@example.com')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = " "
+  test 'name should be present' do
+    @user.name = ' '
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = " "
+  test 'email should be present' do
+    @user.email = ' '
     assert_not @user.valid?
   end
 end
