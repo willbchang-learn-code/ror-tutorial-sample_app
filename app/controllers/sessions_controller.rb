94ae2756fc9f4ab8b871cfe5b78042e0cea74894
Will B Chang <will.b.chang@gmail.com>
2020-09-19 20:56:15 +0800

    fix: avoid warning on login page still see in others

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
     else
       # Create an error message
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
