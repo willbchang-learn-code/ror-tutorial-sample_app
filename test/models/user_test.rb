534d2ebbad812f333b2b2c3023b782dceaa6369f
Will B Chang <will.b.chang@gmail.com>
2020-09-20 21:23:27 +0800

    feat: user logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com',
-                     password: 'foobar', password_confirmation: 'foobar')
+    @user = users(:michael)
   end
 
   test 'should be valid' do
